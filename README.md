8db72596e5cd24d0a9cb4886ca980d506758152e
FuzzyYeti <fuzzyyeti@users.noreply.github.com>
2024-06-04 17:23:09 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Query Grape Governance Directory Realms
+
+## Steps to run
+
+Install dependencies
+```bash
+yarn
+```
+
+Run it with ts-node
+```bash
+yarn dev 
+```
---
+++ index.ts
@@ -0,0 +1,12 @@
+import {createUmi} from "@metaplex-foundation/umi-bundle-defaults";
+import {getChildren, getEntryAddressFromName, getRealmAddressFromName, getRealms, RequestStatus} from "gspl-directory";
+import {publicKey, unwrapOption} from "@metaplex-foundation/umi";
+
+const CONFIG = publicKey("GrVTaSRsanVMK7dP4YZnxTV6oWLcsFDV1w6MHGvWnWCS");
+
+(async () => {
+    const umi = createUmi("your rpc")
+    const entries = await getRealms(umi, CONFIG, RequestStatus.Approved);
+    console.log("Entries", entries);
+    console.log(entries.length)
+})();
